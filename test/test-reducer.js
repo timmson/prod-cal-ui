1577be6b38bf7212e1f059301cc3ad35241bfa31
timmson <timmson666@mail.ru>
2022-02-12 22:07:12 +0300

    Add tests #4

--- test/test-reducer.js
+++ test/test-reducer.js
@@ -1,29 +1,31 @@
 import Reducer from "../src/reducer"
 import {CHANGE_MONTH, CHANGE_YEAR} from "../src/actions"
 
+const getTestCaseDescription = (t, i) => `# ${i} - ${JSON.stringify(t.action)} when state:${JSON.stringify(t.state)} and return:${JSON.stringify(t.expected)}`
+
 describe("Reducer should", () => {
 
-	const testCases = [
-		[
-			{type: "do nothing"},
-			{},
-			{}
-		],
-		[
-			{type: CHANGE_MONTH, value: 2},
-			{year: 1, month: 1},
-			{year: 1, month: 2}
-		],
-		[
-			{type: CHANGE_YEAR, value: 2},
-			{year: 1, month: 1},
-			{year: 2, month: 1}
-		]
-	]
-
-	test.each(testCases)("%s from %s to %s", (action, arrange, expected) => {
-		const actual = Reducer(arrange, action)
-		expect(actual).toEqual(expected)
+	[
+		{
+			state: {},
+			action: {type: "do nothing"},
+			expected: {}
+		},
+		{
+			state: {year: 1, month: 1},
+			action: {type: CHANGE_MONTH, value: 2},
+			expected: {year: 1, month: 2}
+		},
+		{
+			action: {type: CHANGE_YEAR, value: 2},
+			state: {year: 1, month: 1},
+			expected: {year: 2, month: 1}
+		}
+	].map((t, i) => {
+		test(getTestCaseDescription(t, i), () => {
+			const actual = Reducer(t.state, t.action)
+			expect(actual).toEqual(t.expected)
+		})
 	})
 
 })
