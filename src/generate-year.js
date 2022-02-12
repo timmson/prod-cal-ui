714a3c818939df65dcc2cb1d2da5037cb7f688ed
timmson <timmson666@mail.ru>
2022-02-13 01:46:56 +0300

    Split and test generate year #6

--- src/generate-year.js
+++ src/generate-year.js
@@ -1,10 +1,12 @@
 import Moment from "moment"
 import _ from "lodash"
-import Calendar, {DAY_HOLIDAY, DAY_WORK_REDUCED, DAY_WORK} from "prod-cal"
+import Calendar, {DAY_HOLIDAY, DAY_WORK, DAY_WORK_REDUCED} from "prod-cal"
+
 const calendar = new Calendar("ru")
 
 const DAYS_IN_WEEK = 7
 const WEEKS_IN_MONTH = 6
+const MONTHS_IN_YEAR = 12
 const HOURS_IN_REDUCED_DAY = 7
 const HOURS_IN_STANDARD_DAY = 8
 
@@ -12,54 +14,61 @@ const addDays = (moment, days) => moment.add(days, "d")
 const formatToDay = (moment) => moment.format("D")
 const startOfMonth = (moment) => moment.startOf("month")
 const addDaysAndFormatToDay = (moment, days) => formatToDay(addDays(moment, days))
+const sequenceTo = (length, start) =>
+	Array.from(Array(length).keys()).map((i) => i + (start || 0))
+const sequenceFromOneTo = (length) => sequenceTo(length, 1)
 
-const calculateWorkingDaysAndHoursInMonth = (m) => {
-	const summary  = _.countBy(m)
-	return {
-		days: m.length - summary[DAY_HOLIDAY],
-		hours: summary[DAY_WORK] * HOURS_IN_STANDARD_DAY + (summary[DAY_WORK_REDUCED] || 0) * HOURS_IN_REDUCED_DAY
-	}
-}
 
-const generateDay = (momentMonth, cDay, m) => {
-	if (cDay >= 0 && cDay < m.length) {
-		return {
-			date: cDay + 1,
-			type: m[cDay]
-		}
-	} else {
-		return {
-			date: (cDay < 0) ?
-				addDaysAndFormatToDay(startOfMonth(momentMonth), cDay) :
-				addDaysAndFormatToDay(momentMonth.endOf("month"), cDay - m.length + 1),
-			type: "inactive"
-		}
-	}
-}
-
-const generateWeek = (momentMonth, monthStartsInDayOfWeek, i, m) => {
-	const daysFromStartOfTheMonth = i * DAYS_IN_WEEK - monthStartsInDayOfWeek
-	return {
-		number: addDays(startOfMonth(momentMonth.clone()), daysFromStartOfTheMonth).week(),
-		days: Array.from(Array(DAYS_IN_WEEK).keys()).map(
-			(j) => generateDay(momentMonth.clone(), daysFromStartOfTheMonth + j, m)
-		)
-	}
-}
-
-const generateMonth = (year, month, m) => {
-	const momentMonth = Moment([year, month])
+const generateMonth = (year, month) => {
+	const prodCalendar = calendar.getCalendar(year, month)
+	const momentMonth = Moment([year, month - 1])
 	const monthStartsInDayOfWeek = startOfMonth(momentMonth.clone()).format("d") - 1
+
+	const calculateWorkingDaysAndHoursInMonth = () => {
+		const summary = _.countBy(prodCalendar)
+		return {
+			days: prodCalendar.length - summary[DAY_HOLIDAY],
+			hours: summary[DAY_WORK] * HOURS_IN_STANDARD_DAY + (summary[DAY_WORK_REDUCED] || 0) * HOURS_IN_REDUCED_DAY
+		}
+	}
+
+	const generateWeek = (daysFromStartOfTheMonth) => {
+		const generateDay = (momentMonth, dayNumber) => {
+			if (dayNumber >= 0 && dayNumber < prodCalendar.length) {
+				return {
+					date: dayNumber + 1,
+					type: prodCalendar[dayNumber]
+				}
+			} else {
+				return {
+					date: (dayNumber < 0) ?
+						addDaysAndFormatToDay(startOfMonth(momentMonth), dayNumber) :
+						addDaysAndFormatToDay(momentMonth.endOf("month"), dayNumber - prodCalendar.length + 1),
+					type: "inactive"
+				}
+			}
+		}
+
+		return {
+			number: addDays(startOfMonth(momentMonth.clone()), daysFromStartOfTheMonth).week(),
+			days: sequenceTo(DAYS_IN_WEEK).map(
+				(j) => generateDay(momentMonth.clone(), daysFromStartOfTheMonth + j)
+			)
+		}
+	}
+
+
 	return {
 		number: momentMonth.format("M"),
 		name: momentMonth.format("MMMM"),
-		working: calculateWorkingDaysAndHoursInMonth(m),
-		weeks: Array.from(Array(WEEKS_IN_MONTH).keys()).map(
-			(i) => generateWeek(momentMonth, monthStartsInDayOfWeek, i, m)
+		working: calculateWorkingDaysAndHoursInMonth(),
+		weeks: sequenceTo(WEEKS_IN_MONTH).map(
+			(weekNumber) =>
+				generateWeek(weekNumber * DAYS_IN_WEEK - monthStartsInDayOfWeek)
 		)
 	}
 }
 
 export default function GenerateYear(year) {
-	return calendar.getCalendar(year).map((m, month) => generateMonth(year, month, m))
+	return sequenceFromOneTo(MONTHS_IN_YEAR).map((month) => generateMonth(year, month))
 }
--- test/test-generate-year.js
+++ test/test-generate-year.js
@@ -1,6 +1,6 @@
 import GenerateYear from "../src/generate-year"
 
-describe("GenerateCalendar should", () => {
+describe("GenerateYear should", () => {
 
 	test("generate production calendar of the year", () => {
 		const year = GenerateYear(2021)
